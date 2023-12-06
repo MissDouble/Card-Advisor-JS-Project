2a91c86bb06565554e8eabb0b3e0cb8ec50861be
Angela Li <angela.tylee25@gmail.com>
2023-12-06 18:20:54 +0800

    landing-page and header RWD

--- main.js
+++ main.js
@@ -2,3 +2,13 @@ import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
 console.log("Hello world!");
+
+// added by angela
+document.addEventListener('DOMContentLoaded', function () {
+    const mobileMenuButton = document.getElementById('mobile-menu');
+    const menu = document.querySelector('.menu');
+
+    mobileMenuButton.addEventListener('click', function () {
+        menu.classList.toggle('show');
+    });
+});
